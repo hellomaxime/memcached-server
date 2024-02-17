7ac68d34718beb546e2107cf2ff1f727bdd1c0cd
hellomaxime <86252821+hellomaxime@users.noreply.github.com>
2024-02-17 15:38:50 +0100

    support expiry of keys

--- Server.java
+++ Server.java
@@ -9,6 +9,7 @@ import java.util.HashMap;
 public class Server {
 
     private HashMap<String, String[]> store = new HashMap<>(); // <key, [value, flags, byte_count]>
+    private HashMap<String, Long> expired = new HashMap<>();
 
     private int PORT;
 
@@ -25,7 +26,7 @@ public class Server {
             return;
         }
 
-        if (command.matches("set [a-zA-Z0-9]* [0-9]* [0-9]* [0-9]*[ a-zA-Z]*?\\\\r\\\\n")) {
+        if (command.matches("set [a-zA-Z0-9]* [0-9]* [-0-9]* [0-9]*[ a-zA-Z]*?\\\\r\\\\n")) {
             String[] split = command.replace("\\r\\n", "").split(" ");
             String value = in.readLine();
             while(!value.matches("[a-zA-Z0-9]*\\\\r\\\\n")) {
@@ -34,17 +35,38 @@ public class Server {
             }
             String storeValue = value.replace("\\r\\n", "");
             String[] setInfo = new String[]{storeValue, split[2], split[4]};
+
+            // store expiration time
+            if(Integer.parseInt(split[3]) > 0) {
+                long currentTimeSeconds = System.currentTimeMillis() / 1000 + Long.parseLong(split[3]);
+                expired.put(split[1], currentTimeSeconds);
+            } else if(Integer.parseInt(split[3]) < 0) {
+                if(split.length < 6) {
+                    out.println("STORED");
+                }
+                return;
+            }
+
             store.put(split[1], setInfo);
+
             if(split.length < 6) {
                 out.println("STORED");
             }
         } else if(command.matches("get [a-zA-Z0-9]*\\\\r\\\\n")) {
             String[] split = command.replace("\\r\\n", "").split(" ");
+
+            if(expired.containsKey(split[1]) && expired.get(split[1]) < System.currentTimeMillis()/1000) {
+                expired.remove(split[1]);
+                store.remove(split[1]);
+                out.println("END");
+                return;
+            }
+
             if(store.containsKey(split[1])) {
                 String[] getInfo = store.get(split[1]);
                 out.printf("VALUE %s %s %s\r\n%s\r\nEND\r\n", split[1], getInfo[1], getInfo[2], getInfo[0]);
             } else {
-                out.println("ERROR");
+                out.println("END");
             }
         } else {
             out.println("ERROR");
